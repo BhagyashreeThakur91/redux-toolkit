e968a195c638f13b26014d0eb44a945ea8c385b3
BhagyashreeThakur91 <thakurbhagyashree747@gmail.com>
2024-08-05 12:43:16 +0530

    learn redux toolkit

---
+++ src/counter-example/counter-button.jsx
@@ -0,0 +1,18 @@
+import { useDispatch } from 'react-redux'
+import { handleIncreaseCountAction } from '../store/slices/counter';
+function CounterButton() {
+
+    const dispatch = useDispatch()
+
+    function handleClick() {
+        dispatch(handleIncreaseCountAction({
+            id : 1,
+            name : "Bhagyashree"
+        }))
+    }
+
+    return <button
+    onClick={handleClick}
+    className="bg-black text-white font-bold">Increase Count</button>
+}
+export default CounterButton;
